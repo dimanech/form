ba3c26f25af2d07843f797be360139f6148bd09b
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2015-03-31 23:27:32 +0300

    Making form closer to vulyk requirements

--- config.rb
+++ config.rb
@@ -12,7 +12,7 @@ end
 # Page options, layouts, aliases and proxies
 ################################################################################
 
-with_layout :main do
+with_layout :minimal do
   page "*"
 end
 
