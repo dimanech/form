b3c346a682fc235b38baffd1792a5ef6a30415d9
Dmytro Nechepurenko <d.nechepurenko@astoundcommerce.com>
2015-04-11 18:10:56 +0300

    WORKON: autocomplite fix. Feels like it not destroy data properly.

--- source/js/main.js
+++ source/js/main.js
@@ -113,10 +113,36 @@ scripts.Common = {
 	},
 
 	cloneyaInit: function () {
+		var autoCompliteData = {
+				"#general__last-name": scripts.Data.autocompliteData.lastname,
+				"#general__name": scripts.Data.autocompliteData.firstname,
+				"#general__patronymic": scripts.Data.autocompliteData.patronymic,
+				".general__place_district": scripts.Data.autocompliteData.districts,
+				"#general__post_office": scripts.Data.autocompliteData.offices,
+				".vehicle__35__brand": scripts.Data.autocompliteData.cars,
+				".vehicle__36__brand": scripts.Data.autocompliteData.trucks,
+				".vehicle__37__brand": scripts.Data.autocompliteData.boats,
+				".vehicle__39__brand": scripts.Data.autocompliteData.motos
+			},
+			addAutoComplite = function (selector, data) {
+				$(selector).autocomplete({
+					source: function(request, response) {
+						var results = $.ui.autocomplete.filter(data, request.term);
+						response(results.slice(0, 7));
+					},
+					appendTo: $(selector).parent()
+				});
+			};
+
+		$.each (autoCompliteData, function (id, data) {
+			addAutoComplite(id, data);
+		});
+
+
+
 		var $clonewrapper = $('.js-clone-wrapper');
 
 		$clonewrapper.cloneya({
-			limit: 2000,
 			cloneThis: '.js-toclone',
 			valueClone: false,
 			dataClone: false,
@@ -126,21 +152,50 @@ scripts.Common = {
 			clonePosition: 'after',
 			serializeID: false,
 			ignore: '.weiss-form__msg, .js-clone-ignore, .ui-autocomplete',
-			defaultRender: true,
 			preserveChildCount: true
+		}).on('clone_after_append', function (event, toclone, newclone) {
+			var $container = $(newclone).parent('.js-clone-wrapper');
+
+			$(newclone).addClass('js-cloned');
+
+			$.each(autoCompliteData, function(element, data) {
+				var $elem = $container.find(element);
+
+				if ($elem.length > 0) {
+					$elem.autocomplete('destroy');
+					addAutoComplite($elem, data);
+				}
+
+			});
 		});
 	},
 
-	addAutoComplete: function(elem, source) {
-		var $elemPar = $(elem).parent();
-
-		$(elem).autocomplete({
-			source: function(request, response) {
-				var results = $.ui.autocomplete.filter(source, request.term);
-
-				response(results.slice(0, 7));
+	autoCompleteInit: function() {
+		var autoCompliteData = {
+				"#general__last-name": scripts.Data.autocompliteData.lastname,
+				"#general__name": scripts.Data.autocompliteData.firstname,
+				"#general__patronymic": scripts.Data.autocompliteData.patronymic,
+				".general__place_district": scripts.Data.autocompliteData.districts,
+				"#general__post_office": scripts.Data.autocompliteData.offices,
+				".vehicle__35__brand": scripts.Data.autocompliteData.cars,
+				".vehicle__36__brand": scripts.Data.autocompliteData.trucks,
+				".vehicle__37__brand": scripts.Data.autocompliteData.boats,
+				".vehicle__39__brand": scripts.Data.autocompliteData.motos
 			},
-			appendTo: $elemPar
+			addAutoComplite = function (selector, data) {
+				$(selector).each(function(){
+					$(this).autocomplete({
+						source: function(request, response) {
+							var results = $.ui.autocomplete.filter(data, request.term);
+							response(results.slice(0, 7));
+						},
+						appendTo: $(selector).parent()
+					});
+				});
+			};
+
+		$.each (autoCompliteData, function (id, data) {
+			addAutoComplite(id, data);
 		});
 	},
 
@@ -256,40 +311,47 @@ scripts.Common = {
 		scrpt.globalInit();
 
 		$(function () { // DOM Ready
-			var template = Handlebars.compile($('#decl_form_template').html()),
-				output = $("#form-wrapper");
+			scrpt.jqueryValidateInit();
+			scrpt.toggleFormSection();
+			scrpt.inputActions();
+			scrpt.cloneyaInit();
+			scrpt.dateSelectBoxesInit();
+			//scrpt.autoCompleteInit();
 
-			scrpt.$cache.body.on("vulyk.next", function(e, data) {
-				scrpt.$cache.html.scrollTop(0);
-				output.html(template(data.result.task.data));
-				scrpt.jqueryValidateInit();
-				scrpt.toggleFormSection();
-				scrpt.inputActions();
-				scrpt.cloneyaInit();
-				scrpt.dateSelectBoxesInit();
-				scrpt.addAutoComplete("#general__last-name", scripts.Data.autocompliteData.lastname);
-				scrpt.addAutoComplete("#general__name", scripts.Data.autocompliteData.firstname);
-				scrpt.addAutoComplete("#general__patronymic", scripts.Data.autocompliteData.patronymic);
-				scrpt.addAutoComplete("#general__place_district", scripts.Data.autocompliteData.districts);
-				scrpt.addAutoComplete("#general__post_office", scripts.Data.autocompliteData.offices);
-				scrpt.addAutoComplete("#vehicle__35__brand", scripts.Data.autocompliteData.cars);
-				scrpt.addAutoComplete("#vehicle__36__brand", scripts.Data.autocompliteData.trucks);
-				scrpt.addAutoComplete("#vehicle__37__brand", scripts.Data.autocompliteData.boats);
-				scrpt.addAutoComplete("#vehicle__39__brand", scripts.Data.autocompliteData.motos);
-			}).on("vulyk.save", function(e, callback) {
-				var $form = $('#form-declaration'),
-					data = $form.serializeJSON();
-
-				if ($("#section-4").is(":visible") || $("#intro__isnotdeclaration").is(":checked")) {
-					$form.remove();
-					callback(data);
-				} else {
-					scrpt.$cache.html.scrollTop(0);
-				}
-			}).on("vulyk.skip", function(e, callback) {
-				$('#form-declaration').remove();
-				callback();
-			});
+			//var template = Handlebars.compile($('#decl_form_template').html()),
+			//	output = $("#form-wrapper");
+			//
+			//scrpt.$cache.body.on("vulyk.next", function(e, data) {
+			//	scrpt.$cache.html.scrollTop(0);
+			//	output.html(template(data.result.task.data));
+			//	scrpt.jqueryValidateInit();
+			//	scrpt.toggleFormSection();
+			//	scrpt.inputActions();
+			//	scrpt.cloneyaInit();
+			//	scrpt.dateSelectBoxesInit();
+			//	scrpt.addAutoComplete("#general__last-name", scripts.Data.autocompliteData.lastname);
+			//	scrpt.addAutoComplete("#general__name", scripts.Data.autocompliteData.firstname);
+			//	scrpt.addAutoComplete("#general__patronymic", scripts.Data.autocompliteData.patronymic);
+			//	scrpt.addAutoComplete("#general__place_district", scripts.Data.autocompliteData.districts);
+			//	scrpt.addAutoComplete("#general__post_office", scripts.Data.autocompliteData.offices);
+			//	scrpt.addAutoComplete("#vehicle__35__brand", scripts.Data.autocompliteData.cars);
+			//	scrpt.addAutoComplete("#vehicle__36__brand", scripts.Data.autocompliteData.trucks);
+			//	scrpt.addAutoComplete("#vehicle__37__brand", scripts.Data.autocompliteData.boats);
+			//	scrpt.addAutoComplete("#vehicle__39__brand", scripts.Data.autocompliteData.motos);
+			//}).on("vulyk.save", function(e, callback) {
+			//	var $form = $('#form-declaration'),
+			//		data = $form.serializeJSON();
+			//
+			//	if ($("#section-4").is(":visible") || $("#intro__isnotdeclaration").is(":checked")) {
+			//		$form.remove();
+			//		callback(data);
+			//	} else {
+			//		scrpt.$cache.html.scrollTop(0);
+			//	}
+			//}).on("vulyk.skip", function(e, callback) {
+			//	$('#form-declaration').remove();
+			//	callback();
+			//});
 		});
 
 		return scrpt;
